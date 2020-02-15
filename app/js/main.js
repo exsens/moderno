e4f87a425059b48587662714b07b1296d83d894c
exsens <knyazex@mail.ru>
2020-02-15 15:37:47 +0300

    added products slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,22 @@
 $(function(){
+ 
+    $('.rate-star').rateYo({
+        normalFill: '#ffa726',
+        starWidth: '12px',
+        spacing: '2px',
+        readOnly: true,
+        rating: 5,
+    });
 
     var mixer = mixitup('.products__inner-box');
+
+    $('.slider-products__items').slick({
+        
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false,
+        dots: true,
+        dotsClass: 'products__slider-dots',
+      });
     
 });
